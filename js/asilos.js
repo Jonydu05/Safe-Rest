9daec6ae352936d3d2f0d242c4a99d2f56410690
Jonydu05 <joaog.zamp@gmail.com>
2022-10-26 19:12:52 -0300

    mexendo nos scripts do site
    alterei algumas coisas nos scripts e adicionei um json dos asilos

--- js/asilos.js
+++ js/asilos.js
@@ -1,77 +1,101 @@
-  asilos = [
-    serenidade = [
-      nome= 'Espaço Serenidade',
-        endereco =  "Rua das Palmeiras, 290 - Jardim, Santo André - SP",
-        telefone = "11 2325-7112",
-        whatsapp =  "11 99658-6771",
-        descricaoCompleta = "Estamos localizados em área nobre e privilegiada, ao lado do Parque Celso Daniel, em Santo André/SP. O Espaço Serenidade possui ótima infra estrutura, acomodações acolhedoras, ventiladas e iluminação natural. Destaque especial aos amplos jardins em palmeiras imperiais, árvores frutíferas, áreas verdes onde ouvimos o canto dos pássaros e recebemos a visita do beija-flor.",
-        descricaoSimples =  "Estamos localizados em área nobre e privilegiada, ao lado do Parque Celso Daniel, em Santo André/SP. O Espaço Serenidade possui ótima..."
-      ],
-      caminho =  [
-        nome = "Casa Luz do Caminho",
-        endereco = "Rua Domingos José Ferreira, 80 - Mandaqui, São Paulo - Zona norte",
-        telefone = "11 2232-3264",
-        email = "contato@casaluzdocaminho.org.br",
-        descricaoCompleta = "A casa luz do caminho tem por objetivo fornecer a esses irmãozinhos tudo que eles podem esperar de um verdadeiro lar =  alimentação adequada, recreação, assistência médica e odontológica (utilizamos o SUS ou atendimento voluntário) e, acima de tudo, muito amor, carinho e respeito.",
-        descricaoSimples = "A casa luz do caminho tem por objetivo fornecer a esses irmãozinhos tudo que eles podem esperar de um verdadeiro lar..."
-      ],
-      ciai = [
-        nome = "Centro Integrado de Atendimento ao Idoso",
-        endereco = "R. Alfredo Mendes da Silva, 201 – Jardim Jussara, São Paulo - Zona sul",
-        telefone = "11 3751-4951",
-        descricaoCompleta = "Criado há mais de 30 anos, o Centro Integrado de Atendimento ao Idoso se tornou uma referência em residência permanente de cuidados sênior em São Paulo. As suítes possuem infraestrutura completa para a saúde e o conforto dos hóspedes, as áreas de convivência são cercadas de natureza e o canto dos pássaros nos faz esquecer que estamos na cidade. O CIAI possui facilidades como restaurante, café e farmácia, entre outros. A agenda dos nossos hóspedes seniores é repleta de atividades para estimulação cognitiva, física e social, tudo isso orquestrado por uma equipe multidisciplinar, treinada para agir com profissionalismo, mas sem deixar o carinho e o afeto de lado. Seja bem-vindo (a) ao CIAI.",
-        descricaoSimples = "Criado há mais de 30 anos, o Centro Integrado de Atendimento ao Idoso se tornou uma referência em residência permanente de cuidados sênior em São Paulo..."
-      ],
-      esplendor = [
-        nome = "Esplendor Residencial Sênior",
-        endereço =  "Rua Nunes Balboa, 622 - Vila Carrão, São Paulo",
-        telefone =  "11 2097-0440",
-        whatsapp =  "11 98693-6991",
-        descricaoCompleta =  "Próximo a um dos bairros com uma das mais elevadas expectativas de vida em São Paulo o Esplendor Residencial Sênior fica situado no bairro da Vila Carrão, Zona Leste de São Paulo. Seu posicionamento estratégico permite fácil localização e acesso a habitação e cuidado a cidadãos de idade avançada. Sendo assim, o Esplendor Residencial Sênior é a habitação ideal para o idoso que precisa de cuidados especiais, desde alimentação, entretenimento ou atendimento médico ou acompanhamento constante, garantindo a segurança para a família inteira.",
-        descricaoSimples =  "Próximo a um dos bairros com uma das mais elevadas expectativas de vida em São Paulo o Esplendor Residencial Sênior fica situado..."
-      ],
-      garden = [
-        nome = "Gardenville",
-        endereco = "Rua Itá, 381 - São Paulo - Zona norte",
-        whatsapp = "11 93463-5474",
-        descricaoCompleta = "Um residencial para idosos em todos os níveis de necessidades. Tudo aqui foi pensado e desenvolvido para que os moradores se sintam em casa. Por isso, nossas instalações e serviços foram desenhados para valorizar a privacidade e seu bem-estar individual. Nosso atendimento é personalizado de acordo com as necessidades e preferências pessoais de cada morador.",
-        descricaoSimples = "Um residencial para idosos em todos os níveis de necessidades. Tudo aqui foi pensado e desenvolvido para que os moradores se sintam em casa..."
-      ],
-      horto =  [
-        nome = "Clínica de Repouso Horto Florestal",
-        endereco = "Av. Luis Carlos Gentile de Laet, 452 - Horto Florestal, São Paulo",
-        telefone = "11 2203-0701 / 11 2203-0801",
-        whatsapp = "11 93463-3205",
-        descricaoCompleta = "A Clínica de Repouso Horto Florestal, fundada em 1972 pelo médico Dr. Chafik Curi, cuida de pessoas em diferentes condições de saúde. Com amor, respeito e profissionalismo, oferece aos seus residentes um verdadeiro lar. É provavelmente a região urbana com maior densidade de área verde no município. Junta-se à vegetação das ruas e terrenos as extensas matas do Horto Florestal e do Parque Estadual da Cantareira, que ficam em volta - parte do parque pertence ao distrito vizinho Mandaqui.",
-        descricaoSimples = "A Clínica de Repouso Horto Florestal, fundada em 1972 pelo médico Dr. Chafik Curi, cuida de pessoas em diferentes condições de saúde..."
-      ],
-      palmeiras = [
-        nome = "Jardim das Palmeiras",
-        endereco = "Av. Ver. José Diniz, 1448 - Alto da Boa Vista - São Paulo",
-        telefone = "11 5531-5447",
-        descricaoCompleta = "O Residencial Jardim das Palmeiras, um lar para idosos, com mais de 22 anos de experiência no ramo. Tendo uma equipe formada por bons profissionais em diversas áreas. Atualmente agindo com duas unidades, uma no Alto da Boa Vista que é um bairro nobre situado no distrito de Santo Amaro, na zona sul da capital paulista. E outra unidade posicionada no extremo leste do Estado de São Paulo na Região Administrativa de São José dos Campos que fica entre São Paulo e Rio de Janeiro.",
-        descricaoSimples = "O Residencial Jardim das Palmeiras, um lar para idosos, com mais de 22 anos de experiência no ramo. Tendo uma equipe formada por bons profissionais..."
-      ],
-      saoJudas = [
-        nome = "São Judas Residence",
-        endereco = "Rua Vitória Régia, 1204 – Santo André – SP",
-        telefone = "11 4421-4377 / 11 4473-2630",
-        descricaoCompleta = "A São Judas Residence é um espaço totalmente dedicado para idosos e oferece um conceito diferenciado, com ambiente acolhedor e equipe especializada. Localizado em Santo André, um município brasileiro da Região do Grande ABC, localizado na Zona Sudeste da Grande São Paulo, parte da Região Metropolitana de São Paulo.",
-        descricaoSimples = "A São Judas Residence é um espaço totalmente dedicado para idosos e oferece um conceito diferenciado, com ambiente acolhedor..."
-      ],
-      sonhos = [
-        nome = "Residencial Vila dos Sonhos",
-        endereco = "Av. Eng. Caetano Álvares, 6705 – Mandaqui, São Paulo - Zona norte",
-        telefone = "11 2283-2934",
-        descricaoCompleta = "Conheça a Casa de Repouso Residencial Vila dos Sonhos, nosso foco é atender você com serviços inovadores, entre em contato e confira os serviços e profissionais. Contamos com profissionais experientes e treinados periodicamente e por isso, estamos crescendo a cada dia mais.",
-        descricaoSimples = "Conheça a Casa de Repouso Residencial Vila dos Sonhos, nosso foco é atender você com serviços inovadores, entre em contato..."
-      ],
-      vida = [
-        nome = "Residencial Mais Vida",
-        endereco = "R. Afonso Celso, 338 – Vila Mariana, São Paulo - Zona norte",
-        telefone = "11 5081-4059",
-        descricaoCompleta = "A Mais Vida Casa de Repouso funciona em regime residencial e acolhe idosos de ambos os sexos, faixa etária acima de 60 anos, se dedica a acolher idosos para fins de possibilitar que o acolhido seja fortalecido e estimulado a exercer seu papel no mundo. Acolher idosos garantindo proteção, integridade, felicidade e principalmente assegurar seus direitos fundamentais, nos termos de Lei nº 10.741/94",
-        descricaoSimples = "A Mais Vida Casa de Repouso funciona em regime residencial e acolhe idosos de ambos os sexos, faixa etária acima de 60 anos..."
-      ]
-  
-  ]
+export default asilos;
+const asilos = [
+  (serenidade = [
+    (nome = "Espaço Serenidade"),
+    (endereco = "Rua das Palmeiras, 290 - Jardim, Santo André - SP"),
+    (telefone = "11 2325-7112"),
+    (whatsapp = "11 99658-6771"),
+    (descricaoCompleta =
+      "Estamos localizados em área nobre e privilegiada, ao lado do Parque Celso Daniel, em Santo André/SP. O Espaço Serenidade possui ótima infra estrutura, acomodações acolhedoras, ventiladas e iluminação natural. Destaque especial aos amplos jardins em palmeiras imperiais, árvores frutíferas, áreas verdes onde ouvimos o canto dos pássaros e recebemos a visita do beija-flor."),
+    (descricaoSimples =
+      "Estamos localizados em área nobre e privilegiada, ao lado do Parque Celso Daniel, em Santo André/SP. O Espaço Serenidade possui ótima..."),
+  ]),
+  (caminho = [
+    (nome = "Casa Luz do Caminho"),
+    (endereco =
+      "Rua Domingos José Ferreira, 80 - Mandaqui, São Paulo - Zona norte"),
+    (telefone = "11 2232-3264"),
+    (email = "contato@casaluzdocaminho.org.br"),
+    (descricaoCompleta =
+      "A casa luz do caminho tem por objetivo fornecer a esses irmãozinhos tudo que eles podem esperar de um verdadeiro lar =  alimentação adequada, recreação, assistência médica e odontológica utilizamos o SUS ou atendimento voluntário) e, acima de tudo, muito amor, carinho e respeito."),
+    (descricaoSimples =
+      "A casa luz do caminho tem por objetivo fornecer a esses irmãozinhos tudo que eles podem esperar de um verdadeiro lar..."),
+  ]),
+  (ciai = [
+    (nome = "Centro Integrado de Atendimento ao Idoso"),
+    (endereco =
+      "R. Alfredo Mendes da Silva, 201 – Jardim Jussara, São Paulo - Zona sul"),
+    (telefone = "11 3751-4951"),
+    (descricaoCompleta =
+      "Criado há mais de 30 anos, o Centro Integrado de Atendimento ao Idoso se tornou uma referência em residência permanente de cuidados sênior em São Paulo. As suítes possuem infraestrutura completa para a saúde e o conforto dos hóspedes, as áreas de convivência são cercadas de natureza e o canto dos pássaros nos faz esquecer que estamos na cidade. O CIAI possui facilidades como restaurante, café e farmácia, entre outros. A agenda dos nossos hóspedes seniores é repleta de atividades para estimulação cognitiva, física e social, tudo isso orquestrado por uma equipe multidisciplinar, treinada para agir com profissionalismo, mas sem deixar o carinho e o afeto de lado. Seja bem-vindo a) ao CIAI."),
+    (descricaoSimples =
+      "Criado há mais de 30 anos, o Centro Integrado de Atendimento ao Idoso se tornou uma referência em residência permanente de cuidados sênior em São Paulo..."),
+  ]),
+  (esplendor = [
+    (nome = "Esplendor Residencial Sênior"),
+    (endereço = "Rua Nunes Balboa, 622 - Vila Carrão, São Paulo"),
+    (telefone = "11 2097-0440"),
+    (whatsapp = "11 98693-6991"),
+    (descricaoCompleta =
+      "Próximo a um dos bairros com uma das mais elevadas expectativas de vida em São Paulo o Esplendor Residencial Sênior fica situado no bairro da Vila Carrão, Zona Leste de São Paulo. Seu posicionamento estratégico permite fácil localização e acesso a habitação e cuidado a cidadãos de idade avançada. Sendo assim, o Esplendor Residencial Sênior é a habitação ideal para o idoso que precisa de cuidados especiais, desde alimentação, entretenimento ou atendimento médico ou acompanhamento constante, garantindo a segurança para a família inteira."),
+    (descricaoSimples =
+      "Próximo a um dos bairros com uma das mais elevadas expectativas de vida em São Paulo o Esplendor Residencial Sênior fica situado..."),
+  ]),
+  (garden = [
+    (nome = "Gardenville"),
+    (endereco = "Rua Itá, 381 - São Paulo - Zona norte"),
+    (whatsapp = "11 93463-5474"),
+    (descricaoCompleta =
+      "Um residencial para idosos em todos os níveis de necessidades. Tudo aqui foi pensado e desenvolvido para que os moradores se sintam em casa. Por isso, nossas instalações e serviços foram desenhados para valorizar a privacidade e seu bem-estar individual. Nosso atendimento é personalizado de acordo com as necessidades e preferências pessoais de cada morador."),
+    (descricaoSimples =
+      "Um residencial para idosos em todos os níveis de necessidades. Tudo aqui foi pensado e desenvolvido para que os moradores se sintam em casa..."),
+  ]),
+  (horto = [
+    (nome = "Clínica de Repouso Horto Florestal"),
+    (endereco =
+      "Av. Luis Carlos Gentile de Laet, 452 - Horto Florestal, São Paulo"),
+    (telefone = "11 2203-0701 / 11 2203-0801"),
+    (whatsapp = "11 93463-3205"),
+    (descricaoCompleta =
+      "A Clínica de Repouso Horto Florestal, fundada em 1972 pelo médico Dr. Chafik Curi, cuida de pessoas em diferentes condições de saúde. Com amor, respeito e profissionalismo, oferece aos seus residentes um verdadeiro lar. É provavelmente a região urbana com maior densidade de área verde no município. Junta-se à vegetação das ruas e terrenos as extensas matas do Horto Florestal e do Parque Estadual da Cantareira, que ficam em volta - parte do parque pertence ao distrito vizinho Mandaqui."),
+    (descricaoSimples =
+      "A Clínica de Repouso Horto Florestal, fundada em 1972 pelo médico Dr. Chafik Curi, cuida de pessoas em diferentes condições de saúde..."),
+  ]),
+  (palmeiras = [
+    (nome = "Jardim das Palmeiras"),
+    (endereco = "Av. Ver. José Diniz, 1448 - Alto da Boa Vista - São Paulo"),
+    (telefone = "11 5531-5447"),
+    (descricaoCompleta =
+      "O Residencial Jardim das Palmeiras, um lar para idosos, com mais de 22 anos de experiência no ramo. Tendo uma equipe formada por bons profissionais em diversas áreas. Atualmente agindo com duas unidades, uma no Alto da Boa Vista que é um bairro nobre situado no distrito de Santo Amaro, na zona sul da capital paulista. E outra unidade posicionada no extremo leste do Estado de São Paulo na Região Administrativa de São José dos Campos que fica entre São Paulo e Rio de Janeiro."),
+    (descricaoSimples =
+      "O Residencial Jardim das Palmeiras, um lar para idosos, com mais de 22 anos de experiência no ramo. Tendo uma equipe formada por bons profissionais..."),
+  ]),
+  (saoJudas = [
+    (nome = "São Judas Residence"),
+    (endereco = "Rua Vitória Régia, 1204 – Santo André – SP"),
+    (telefone = "11 4421-4377 / 11 4473-2630"),
+    (descricaoCompleta =
+      "A São Judas Residence é um espaço totalmente dedicado para idosos e oferece um conceito diferenciado, com ambiente acolhedor e equipe especializada. Localizado em Santo André, um município brasileiro da Região do Grande ABC, localizado na Zona Sudeste da Grande São Paulo, parte da Região Metropolitana de São Paulo."),
+    (descricaoSimples =
+      "A São Judas Residence é um espaço totalmente dedicado para idosos e oferece um conceito diferenciado, com ambiente acolhedor..."),
+  ]),
+  (sonhos = [
+    (nome = "Residencial Vila dos Sonhos"),
+    (endereco =
+      "Av. Eng. Caetano Álvares, 6705 – Mandaqui, São Paulo - Zona norte"),
+    (telefone = "11 2283-2934"),
+    (descricaoCompleta =
+      "Conheça a Casa de Repouso Residencial Vila dos Sonhos, nosso foco é atender você com serviços inovadores, entre em contato e confira os serviços e profissionais. Contamos com profissionais experientes e treinados periodicamente e por isso, estamos crescendo a cada dia mais."),
+    (descricaoSimples =
+      "Conheça a Casa de Repouso Residencial Vila dos Sonhos, nosso foco é atender você com serviços inovadores, entre em contato..."),
+  ]),
+  (vida = [
+    (nome = "Residencial Mais Vida"),
+    (endereco = "R. Afonso Celso, 338 – Vila Mariana, São Paulo - Zona norte"),
+    (telefone = "11 5081-4059"),
+    (descricaoCompleta =
+      "A Mais Vida Casa de Repouso funciona em regime residencial e acolhe idosos de ambos os sexos, faixa etária acima de 60 anos, se dedica a acolher idosos para fins de possibilitar que o acolhido seja fortalecido e estimulado a exercer seu papel no mundo. Acolher idosos garantindo proteção, integridade, felicidade e principalmente assegurar seus direitos fundamentais, nos termos de Lei nº 10.741/94"),
+    (descricaoSimples =
+      "A Mais Vida Casa de Repouso funciona em regime residencial e acolhe idosos de ambos os sexos, faixa etária acima de 60 anos..."),
+  ]),
+];
--- js/scriptAsilos.js
+++
@@ -1,5 +0,0 @@
-import {serenidade} from "asilos.js";
-
-const serenidade = serenidade;
-
-console.log(serenidade);
