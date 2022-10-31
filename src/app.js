a5eb2f7f5dee27aa59e54ae18710c935e9b37dea
Jonydu05 <joaog.zamp@gmail.com>
2022-10-31 01:57:34 -0300

    configuracao final??

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import { search } from "./seletores";
 import { searchInKeyUp } from "./pesquisa";
 import { mostrarTudo} from "./mostrar";
+import { apagarAsilo } from "./apagar";
 import asilos from "./asilos";
 import _ from 'lodash';
 
 search.addEventListener("keyup", _.debounce(searchInKeyUp, 400));
+
+document.body.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const asiloId = event.target.getAttribute("data-remove");
+  if (asiloId) {
+    apagarAsilo(asiloId);
+  }
+});
 
 mostrarTudo(asilos);
