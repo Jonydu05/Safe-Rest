a4ee8d994f2ed5c7d99455457bdd684c10d71534
Jonydu05 <joaog.zamp@gmail.com>
2022-11-04 02:27:22 -0300

    adicionando landing page
    add landing page e alterei o index para ela. a pag de index de antes agora é asilos.html

---
+++ src/scroll.js
@@ -0,0 +1,9 @@
+ScrollReveal({duration: 1300, distance: '200px', origin: 'right', easing: 'ease-in-out'});
+
+ScrollReveal().reveal('.area-1');
+
+ScrollReveal().reveal('.area-2', {origin: 'left', delay: 200});
+
+ScrollReveal().reveal('.area-3',);
+
+ScrollReveal().reveal('.area-4', {origin: 'left', delay: 200});
